2c2a733b1ef233f71de0442a84eb8acc94379a14
AdamHasma <muslim.hasmagomadov@gmail.com>
2021-03-28 19:04:52 +0200

    logo

---
+++ new-site/main.js
@@ -0,0 +1,8 @@
+window.onscroll = function(){
+   if(window.scrollY >= (window.innerHeight)*0.8) {
+      document.querySelector(".nav-logo").src="assets/handgemacht-t.svg";
+   }else {
+      document.querySelector(".nav-logo").src="assets/handgemacht.png";
+    }
+
+};
