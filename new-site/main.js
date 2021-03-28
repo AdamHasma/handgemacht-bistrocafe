fd778ea755ea3b3ff60785e6e5d7cb53c2a9b735
AdamHasma <muslim.hasmagomadov@gmail.com>
2021-03-28 19:25:52 +0200

    asdasd

--- new-site/main.js
+++ new-site/main.js
@@ -1,5 +1,5 @@
 window.onscroll = function(){
-   if(window.scrollY >= (window.innerHeight)*0.8) {
+   if(window.scrollY >= (window.innerHeight)*0.8-120) {
       document.querySelector(".nav-logo").src="assets/handgemacht-t.svg";
    }else {
       document.querySelector(".nav-logo").src="assets/handgemacht.png";
